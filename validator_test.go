342b1606801e8e2fce8be75843c86c72697d0601
juandaantoniusapakpahan <jujupapa06@gmail.com>
2023-05-23 10:21:36 +0700

    tag parameter validator

--- validator_test.go
+++ validator_test.go
@@ -66,8 +66,26 @@ func TestMultipleTagError(t *testing.T) {
 func TestMultipleTagPass(t *testing.T) {
 
 	var validate *validator.Validate = validator.New()
-	data := 12423
+	data := "12423"
 	err := validate.Var(data, "required,numeric")
 
 	assert.Equal(t, nil, err)
+}
+
+func TestTagParameterError(t *testing.T) {
+	var validate *validator.Validate = validator.New()
+	data := "1213"
+
+	err := validate.Var(data, "required,numeric,min=5,max=20")
+
+	fmt.Println(err.Error())
+	assert.NotEqual(t, nil, err)
+}
+
+func TestTagParameterPass(t *testing.T) {
+	var validate *validator.Validate = validator.New()
+	data := "134323432289985"
+	err := validate.Var(data, "required,numeric,min=5,max=20")
+	assert.Equal(t, nil, err)
+
 }
